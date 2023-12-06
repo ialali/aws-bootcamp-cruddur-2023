a69fd1d768cbcc93b1c854da81bdbadfabbf3906
Ibrahim Al-Ali <ibrahim.alali94@yahoo.ro>
2023-12-06 11:50:10 +0000

    added alarm

--- env.sh
+++ env.sh
@@ -1,4 +1,10 @@
 aws budgets create-budget \
-  --account-id $ACCOUNT_ID \
+  --account-id 361053599075 \
   --budget file://aws/json/budget.json \
   --notifications-with-subscribers file://aws/json/budget-notification-with-subscription.json
+
+
+aws sns subscribe \
+  --topic-arn "arn:aws:sns:eu-west-2:361053599075:billing-alarm" \
+  --protocol email \
+  --notification-endpoint ibrahim.alali94@yahoo.ro
